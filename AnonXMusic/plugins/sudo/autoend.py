ebfa754d3499b164a3f11f99312bfc8fa59e1db4
damian-bots <tosu0791@gmail.com>
2025-03-11 02:21:23 +0530

    Update autoend.py

--- AnonXMusic/plugins/sudo/autoend.py
+++ AnonXMusic/plugins/sudo/autoend.py
@@ -22,3 +22,23 @@ async def auto_end_stream(_, message: Message):
         await message.reply_text("» ᴀᴜᴛᴏ ᴇɴᴅ sᴛʀᴇᴀᴍ ᴅɪsᴀʙʟᴇᴅ.")
     else:
         await message.reply_text(usage)
+
+
+@app.on_message(filters.command("autoleave") & SUDOERS)
+async def auto_leave_chat(_, message: Message):
+    zerostate = await is_autoleave()
+    usage = f"<b>ᴇxᴀᴍᴘʟᴇ :</b>\n\n/autoleave [ᴇɴᴀʙʟᴇ | ᴅɪsᴀʙʟᴇ]\n\n Current state : {zerostate}"
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    state = message.text.split(None, 1)[1].strip().lower()
+    if state in ["enable","on","yes"]:
+        await autoleave_on()
+        await message.reply_text(
+            "» ᴀᴜᴛᴏ leave chat ᴇɴᴀʙʟᴇᴅ.\n\nᴀssɪsᴛᴀɴᴛ ᴡɪʟʟ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇᴀᴠᴇ ᴛʜᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀғᴛᴇʀ ғᴇᴡ ᴍɪɴs ᴡʜᴇɴ ɴᴏ ᴏɴᴇ ɪs ʟɪsᴛᴇɴɪɴɢ."
+        )
+    elif state in ["disable","off","no"]:
+        await autoleave_off()
+        await message.reply_text("» ᴀᴜᴛᴏ leave chat ᴅɪsᴀʙʟᴇᴅ.")
+    else:
+        await message.reply_text(usage)
+        
